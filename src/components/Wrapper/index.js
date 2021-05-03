110cc2348e541a386e95310255d6a49c3a7fd5d2
Wanda Gleason <oglissane@gmail.com>
2021-05-02 22:54:21 -0400

    Adding simple wrapper div.

---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Wrapper() {
+  return (
+    <div className="wrapper">
+      <div className="title">Employee Directory</div>
+    </div>
+  );
+}
+
+export default Wrapper;

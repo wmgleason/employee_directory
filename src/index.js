aa6f0e138e43e99609d59476449b6045eef40e42
Wanda Gleason <oglissane@gmail.com>
2021-05-03 01:13:03 -0400

    Another typo.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
+import "./style.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 // https://stackoverflow.com/questions/53737280/how-to-add-service-worker-in-react-project

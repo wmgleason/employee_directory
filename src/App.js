868e24f57184efed7edb51b6f535f5b6923d0b24
Wanda Gleason <oglissane@gmail.com>
2021-05-02 21:11:04 -0400

    Added isLoading state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import axios from "axios";
 import React from "react";
 import Header from "./components/Header";
-import "../styles/main.css";
+import axios from "axios";
 
 export default class App extends React.Component {
+  state = {
+    users: [],
+    isLoading: false,
+    errorMsg: "",
+  };
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    axios
+      .get("https://randomuser.me/api/?page=0&results=25")
+      .then((response) => {
+        this.setState({ users: response.data.results, errorMsg: "" });
+      })
+      .catch((error) =>
+        this.setState({
+          errorMsg: "Error while loading data. Try again later.",
+        })
+      )
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
+
   render() {
+    const { users, isLoading, errorMsg } = this.state;
+    console.log(users);
+
     return (
       <div className="main-section">
         <Header />
-        <h2>Directory</h2>
+        {isLoading && <p className="loading">Loading...</p>}
+        {errorMsg && <p className="errorMsg">{errorMsg}</p>}
       </div>
     );
   }

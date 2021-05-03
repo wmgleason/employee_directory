e902db6338d1824e8be5d43ec684d3821c5029f9
Wanda Gleason <oglissane@gmail.com>
2021-05-03 01:08:59 -0400

    Added import serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+// https://stackoverflow.com/questions/53737280/how-to-add-service-worker-in-react-project
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,9 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));

676b7ded825ad119e4647911f9157df9d7ef4975
Wanda Gleason <oglissane@gmail.com>
2021-05-02 20:53:11 -0400

    Added import styles.css

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 import axios from "axios";
+import React from "react";
+import Header from "./components/Header";
+import "../styles/main.css";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="main-section">
+        <Header />
+        <h2>Directory</h2>
+      </div>
+    );
+  }
+}

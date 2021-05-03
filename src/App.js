7ae7f1d2673a8e9722f38b24d291b18b1ea1c528
Wanda Gleason <oglissane@gmail.com>
2021-05-03 00:42:56 -0400

    Added import wrapper.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import axios from "axios";
-import SiteHead from './components/Header';
-import Navbar from './components/Navbar';
-
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
 
 class App extends React.Component {
-
   state = {
     employees: [],
   };
 
-//   if component mounts, do...
   componentDidMount() {
-      axios.get(`https://randomuser.me/api/?results=25`)
-        .then(res => {
-          this.setState({ employees: res.data.results });
-        });
+    axios.get(`https://randomuser.me/api/?results=25`).then((res) => {
+      this.setState({ employees: res.data.results });
+    });
   }
-
 
   render() {
     return (
       <div className="App">
         <SiteHead />
-        {this.state.employees.length > 0 &&
-        <Navbar employees={this.state.employ
+        {this.state.employees.length > 0 && (
+          <Navbar employees={this.state.employees} />
+        )}
       </div>
     );
   }
